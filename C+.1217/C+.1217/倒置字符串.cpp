dcf1b1ec6f76fb1936df9393d0a65c9b0aba9d3a
STong01 <1461638919@qq.com>
2019-12-18 10:18:30 +0800

    倒置字符串

--- C+.1217/C+.1217/倒置字符串.cpp
+++ C+.1217/C+.1217/倒置字符串.cpp
@@ -6,25 +6,27 @@ using namespace std;
 
 int main()
 {
-	string s;
+	string s;//定义字符串
 
-	getline(cin, s);
+	getline(cin, s);//输入字符串，不能使用cin，cin碰见空格则会停止输入
 
-	reverse(s.begin(), s.end());
+	reverse(s.begin(), s.end());//倒置整体字符串
 
 	auto start = s.begin();
 
 	while (start != s.end())
 	{
+
 		auto end = start;
-		while (end != s.end() && *end != ' ')
+
+		while (end != s.end() && *end != ' ')//查找字符串中的单个字符
 		{
 			end++;
 		}
 
-		reverse(start, end);
+		reverse(start, end);//将单词倒置
 
-		if (end != s.end())
+		if (end != s.end())//转移到下一个未倒置单词
 		{
 			start = end + 1;
 		}
