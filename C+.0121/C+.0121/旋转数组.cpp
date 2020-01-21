0d7dcdb24dbc1d897d618507d70ff255b2169a94
STong01 <1461638919@qq.com>
2020-01-21 23:43:28 +0800

    旋转数组

--- C+.0121/C+.0121/旋转数组.cpp
+++ C+.0121/C+.0121/旋转数组.cpp
@@ -3,23 +3,27 @@
 using namespace std;
 
 //Ðý×ªÊý×é
-class Solution 
-{
-public:
-	void rotate(vector<int>& nums, int k) 
-	{
-		vector<int> tmp = nums;
-		int p = nums.size();
-		for (int i = 0; i < p; i++)
-		{
-			tmp[((i + k) % p)] = nums[i];
-		}
-		nums = tmp;
-	}
-};
+//class Solution 
+//{
+//public:
+//	void rotate(vector<int>& nums, int k) 
+//	{
+//		vector<int> tmp = nums;
+//		int p = nums.size();
+//		for (int i = 0; i < p; i++)
+//		{
+//			tmp[((i + k) % p)] = nums[i];
+//		}
+//		nums = tmp;
+//	}
+//};
 
 int main()
 {
+	int x = 10;
+	int y = 10;
+	x = y = ++y;
+	cout << x << y << endl;
 
 	system("pause");
 
